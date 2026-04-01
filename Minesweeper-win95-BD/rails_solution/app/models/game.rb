3fa54c10115798e11b86492b9e04f7e2bfbc453e
Breno Diogo <142155763+brenodiogo1@users.noreply.github.com>
2026-04-01 13:10:29 +0200

    Delete Minesweeper-win95-BD directory

--- Minesweeper-win95-BD/rails_solution/app/models/game.rb
+++
@@ -1,8 +0,0 @@
-class Game < ApplicationRecord
-  has_many :cells, dependent: :destroy
-  has_one :score, dependent: :destroy
-
-  validates :difficulty, presence: true, inclusion: { in: %w[beginner intermediate expert] }
-  validates :rows, :cols, :mines, presence: true, numericality: { greater_than: 0 }
-  validates :state, presence: true, inclusion: { in: %w[playing won lost] }
-end
